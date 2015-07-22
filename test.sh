c12320e009b43bcdd07f4ec6c701000eed896a4a
Gun Woo Park <gunwoo.park@unina.it>
2015-07-22 18:42:29 +0200

    minor bug fix for file name name change for main trajectory plots

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 
 ./Brownian_simulation test.inp
-python post_processing/plot_position_multiprocessing_association_2d.py data_longer/test.traj data_longer/test.hash data_longer/test.weight data_longer/test.ener data_longer/figures_test 40 1000 12
+python post_processing/plot_association_2d_onestep.py data_longer/test.traj data_longer/test.hash data_longer/test.weight data_longer/test.ener data_longer/figures_test 40 1000 12
 ffmpeg -r 10 -i data_longer/figures_test/t%08d.png -vcodec h264 -vf scale=2400:-1 -pix_fmt yuv420p data_longer/test.mp4
