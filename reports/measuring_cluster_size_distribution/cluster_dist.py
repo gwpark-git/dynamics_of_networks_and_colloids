7ad5e96e44a3a06d7a22aa5f8e7a3e5fa15f90a1
Gun Woo Park <gunwoo.park@unina.it>
2016-01-28 18:22:59 +0100

    UPDATE: Iterative DFS for networks DFS travel for given undirected graph inside PBC box Because of limited depth for recursive call in python, the iterative approach is of importance even if it is dirty compared with recursive one. In addition, the time consumming for iteration typically faster than the one in recursion, which has benefit for boosting up the post-processing time.

--- reports/measuring_cluster_size_distribution/cluster_dist.py
+++ reports/measuring_cluster_size_distribution/cluster_dist.py
@@ -9,7 +9,14 @@ def check_ident(pos, index, target, Ld):
         if(ident_minimum_distance_k_from_x(pos[index, k], pos[target, k], Ld) != 0):
             return 1
     return 0
-                                            
+
+def check_travel_beyond_box(pos, index, target, Ld):
+    Nd = shape(pos)[1]
+    for k in range(Nd):
+        if (ident_minimum_distance_k_from_x(pos[index, k], pos[target, k], Ld) != 0):
+            return 1
+    return 0
+
 
 # def DFS_single_root(hash, stack_component, stack_order, index=0, order_count=1, bp_index=0, cnt=0):
 #     cnt += 1
@@ -255,6 +262,134 @@ def DFS_percolation_edge_restricted_box_test(hash, pos, Ld, stack_component, ind
     except:
         print 'ERR: CNT=%ld, INDEX=%ld, TARGET=%ld, ORDER_CNT=%ld'%(cnt, index, target, order_count)
     return size(queue)
+
+
+def ident_over(hash, index, order_count):
+    N_cols = shape(hash)[1]
+    if order_count >= N_cols:
+        return 1
+    if int(hash[index, order_count]) is -1:
+        return 1
+    return 0
+
+def DF_percolation_edge_restricted_box_iter(hash, pos, Ld, stack_component, index=0, order_count=1, cnt=0, IDPC=[], IDPI=[], queue=[], queue_order=[]):
+    # cnt += 1
+    # const_new_order_count = 1
+    # N_cols = shape(hash)[1]
+    # if size9stack_component)==0:
+    cnt = 0
+    const_new_order_count = 1
+    N_cols = shape(hash)[1]
+    queue.append(int(index))
+    queue_order.append(order_count)
+    try:
+        while(size(queue) > 0):
+            if order_count < N_cols:
+                target = hash[index, order_count]
+                tmp_ID = check_ident(pos, index, target, Ld)
+                if target == -1:
+                    ident_over = True
+                    while(ident_over): # this while phrase is design to capture last queue object also over order_count.                        
+                        if size(queue) == 1:
+                            return 0
+                        else:
+                            queue = queue[:-1]; queue_order = queue_order[:-1]
+                            index = queue[-1]; order_count = queue_order[-1] + 1;
+                            if order_count < N_cols:
+                                ident_over = False
+                elif (target in stack_component) or (tmp_ID == 1):
+                    if tmp_ID == 1:
+                        for id in range(shape(pos[index, :])[0]):
+                            ident_IDP = ident_minimum_distance_k_from_x(pos[index, id], pos[target, id], Ld)
+                            if (int(ident_IDP) is not 0) and ([index, target] not in IDPI):
+                                IDPC.append([id, ident_IDP])
+                                IDPI.append([index, target])
+                    index = index; order_count = order_count + 1;
+                else:
+                    stack_component.append(target)
+                    queue.append(int(target)); queue_order.append(order_count)
+                    index = target; order_count = const_new_order_count;
+            else:
+                ident_over = True
+                while(ident_over):
+                    if size(queue) == 1:
+                        return 0
+                    else:
+                        queue = queue[:-1]; queue_order = queue_order[:-1]
+                        index = queue[-1]; order_count = queue_order[-1] + 1;
+                        if order_count < N_cols:
+                            ident_over = False
+    except:
+        print 'ERR: CNT=%ld, INDEX=%ld, TARGET=%ld, ORDER_CNT=%ld, size_queue=%ld'%(cnt, index, target, order_count, size(queue))
+    return size(queue)
+
+def cluster_edge_DFS_travel_restricted_box_iter(hash, pos, Ld, stack_component, index=0, order_count=1, cnt=0, IDPC=[], IDPI=[], queue=[], queue_order=[]):
+    cnt = 0; const_new_order_count = 1 # initialisation variables
+    N_cols = shape(hash)[1] # limitation for the hash tables
+    queue.append(int(index)); queue_order.append(order_count) # initial queueing
+    while(size(queue) > 0): # will false when size(queue) is 0 if it is not initial step
+        cnt += 1 # temporal counting 
+        ident_over_cols = ident_over(hash, index, order_count)
+        if ident_over_cols: # in the case that the hash[index, order_count] reaching end (-1 or order_count is over)
+            queue = queue[:-1]; queue_order = queue_order[:-1]
+            if (size(queue) > 0):
+                index = queue[-1]; order_count = queue_order[-1] + 1
+        else: # in the case that the hash[index, order_count] is properly defined
+            target = hash[index, order_count]
+            travel_beyond_box = check_travel_beyond_box(pos, index, target, Ld)
+            if (target in stack_component) or travel_beyond_box: # when target is in stack queue or travel beyond box boundary
+                if travel_beyond_box: 
+                    for id in range(shape(pos[index, :])[0]):
+                        ident_IDP = ident_minimum_distance_k_from_x(pos[index, id], pos[target, id], Ld)
+                        if (int(ident_IDP) is not 0) and ([index, target] not in IDPI):
+                            IDPC.append([id, ident_IDP])
+                            IDPI.append([index, target])
+                # when particle is duplicated or travel_beyond_box
+                index = index; order_count = order_count + 1;
+
+                # this means it inherit the exist index for bead but increase order_count
+                # note that the target for next step is given by hash[index, order_count]
+            else: # when the target will stack
+                stack_component.append(int(target))
+                queue.append(int(target)); queue_order.append(order_count) # record element and its order for queue
+                index = target; order_count = const_new_order_count; # depth first search
+    return size(queue)
+
+def print_cluster_info(root_index, IDPC, IDPI, Nd, detail=0):
+    # pL = []; pR = []
+    print 'analysis including root particle %d' % (root_index)
+    flag_left = -1
+    flag_right = 1
+    
+    for n in range(Nd):
+        print '\tfor axis %d'%(n)
+        p_text_left = '\t  travel left pair: '
+        p_text_right = '\t  travel right pair: '
+        cnt_left = 0
+        cnt_right = 0
+        for i in range(shape(IDPC)[0]):
+            if int(IDPC[i][0]) is n:
+                if int(IDPC[i][1]) is flag_left:
+                    if detail:
+                        p_text_left += '(%3d, %3d), '%(IDPI[i][0], IDPI[i][1])
+                    else:
+                        cnt_left += 1
+                elif int(IDPC[i][1]) is flag_right:
+                    if detail:
+                        p_text_right += '(%3d, %3d), '%(IDPI[i][0], IDPI[i][1])
+                    else:
+                        cnt_right += 1
+                else:
+                    flag_default = 0
+        if not(detail):
+            p_text_left += ' %d pairs'%(cnt_left)
+            p_text_right += ' %d pairs'%(cnt_right)
+        print p_text_left
+        print p_text_right
+        if cnt_left > 0 and cnt_right > 0:
+            print '\tcluster with root %d is percolated along %d axis'%(root_index, n)
+    return 0
+                    
 
 
 # sc = []; so = []; IDPC=[]; IDPI=[]
