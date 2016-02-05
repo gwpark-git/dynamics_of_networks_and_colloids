e594632ad4829fb230adc2d35b7245ba4803e05b
Gun Woo Park <gunwoo.park@unina.it>
2016-02-05 16:22:07 +0100

    neutralization branch

--- plt_N_assoc_tmp.py
+++ plt_N_assoc_tmp.py
@@ -1,31 +1,42 @@
 
-from numpy import *
-import matplotlib.pyplot as plt
+# from numpy import *
+# import matplotlib.pyplot as plt
 
-def get_N_assoc(dat, Np):
-    Nt = int(shape(dat)[0]/Np)
-    re = zeros(Nt)
-    for ts in range(Nt):
-        re[ts] = sum(dat[ts*Np:(ts+1)*Np, 1:])/2.0
-    return re
-Np = 640
+# def get_N_assoc(dat, Np):
+#     Nt = int(shape(dat)[0]/Np)
+#     re = zeros(Nt)
+#     for ts in range(Nt):
+#         re[ts] = sum(dat[ts*Np:(ts+1)*Np, 1:])/2.0
+#     return re
+# Np = 3200
 
-dat = loadtxt('data/NP640_C100_T3_r11.weight')
-dat_long = loadtxt('data_MKL_LONG/NP640_C100_T3_r11.weight')
-# dat_INDEX_FIX = loadtxt('data_INDEX_FIX/NP640_C100_T3_r11.weight')
-# # dat_INDEX_FIX_BOOST = loadtxt('data_INDEX_FIX_BOOST/NP640_C100_T3_r11.weight')
-# dat_INDEX_FIX_FLAG = loadtxt('data_INDEX_FIX_FLAG/NP640_C100_T3_r11.weight')
-# N1 = shape(dat_INDEX_FIX_FLAG)[0]/Np
-# # dat_INDEX_FIX_FLAG_longer = loadtxt('data_INDEX_FIX_FLAG_longer/NP640_C100_T3_r11.weight')
-# dat_TEST_longer = loadtxt('data_TEST_longer/NP640_C100_T3_r11.weight')
-# N2 = shape(dat_TEST_longer)[0]/Np
-
+# NAS_100 = get_N_assoc(loadtxt('TEST_3D_NP3200_LD20P3_SF15/NP3200_C100_T3.weight'), Np)
+# NAS_050 = get_N_assoc(loadtxt('TEST_3D_NP3200_LD20P3_SF15_half/NP3200_C100_T3.weight'), Np)
 
 plt.clf()
 plt.ion()
 
-plt.plot(get_N_assoc(dat, Np), 'b-', label = 'data')
-plt.plot(get_N_assoc(dat_long, Np), 'r.', label ='MKL_LONG')
+ax = plt.subplot(111)
+Nt = size(NAS_100)
+dt = 0.001
+N_skip = 100
+
+t = arange(Nt)*dt*N_skip
+Rt = 100
+# t = t/Rt
+
+ax.plot(t[:size(NAS_050)], NAS_050, 'r.', label =r'$R_t=\tau_D/\tau_B=50$')
+ax.plot(t, NAS_100, 'b-', label =r'$R_t=\tau_D/\tau_B=100 $')
+
+# ax.set_xlabel('Tological dimensionless time')
+ax.set_xlabel('Brownian dimensionless time')
+ax.set_ylabel('number of associations')
+ax.legend(loc = 'lower right', numpoints=1)
+ax.grid()
+
+ax2 = ax.twinx()
+ax2.axis([0, t[-1], 0, 4000./(25.*3200.)])
+ax2.set_ylabel('fraction = NAS/80,000')
 # plt.plot(get_N_assoc(dat_INDEX_FIX, Np), 'r-', label = 'INDEX_FIX')
 # plt.plot(get_N_assoc(dat_INDEX_FIX_BOOST, Np), 'b.', label = 'INDEX_FIX_BOOST')
 # plt.plot(arange(N1), get_N_assoc(dat_INDEX_FIX_FLAG, Np), 'b-', label = 'FIX_FLAG') # same with previous
@@ -34,8 +45,4 @@ plt.plot(get_N_assoc(dat_long, Np), 'r.', label ='MKL_LONG')
 # plt.plot(arange(N2)*10, get_N_assoc(dat_TEST_longer, Np), 'r.-', label = 'TEST_longer')
 
 
-plt.xlabel('time step')
-plt.ylabel('number of associations')
-plt.legend(loc = 'lower right', numpoints=1)
-plt.grid()
 plt.show()
