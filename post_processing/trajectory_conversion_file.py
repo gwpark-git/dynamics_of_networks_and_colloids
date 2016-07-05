92b03a9674e830dc4dd871b55a7cce4176f72928
Gun Woo Park <gunwoo.park@unina.it>
2016-07-05 14:35:48 +0200

    [UPDATE, PP] trajectory conversion with simple shear (tracking individual particle with reality) The previous code is tracking the particle with the same index. For given mechanical perturbation, however, the particle is distingushable between its images especially for shear gradient direction when simple shear is applied. Since the trajectory conversion code is design for tracking single particle (realistic manner) in space, it is of importance to more shifting due to the jump of shear gradient direction.
    Former-commit-id: 04e822113973850fe8f4f7c3157bb3e49ffe282a

--- post_processing/trajectory_conversion_file.py
+++ post_processing/trajectory_conversion_file.py
@@ -15,9 +15,11 @@ if size(sys.argv) < 6:
     print 'argv[3] == spatial dimension'
     print 'argv[4] == number of particles'
     print 'argv[5] == box dimension'
-    print 'argv[6] == Wi_tau_C: tau_C is characteristic time to update Langevin Equation'
-    print 'argv[7] == shear_direction (if Wi > 0)'
-    print 'argv[8] == shear_grad_direction (if Wi > 0)'
+    print 'argv[6] == (OPTIONAL) Wi_tau_C: tau_C is characteristic time to update Langevin Equation'
+    print 'argv[7] == (OPTIONAL: FELLOW argv[6]) shear_direction (if Wi > 0)'
+    print 'argv[8] == (OPTIONAL: FELLOW argv[6]) shear_grad_direction (if Wi > 0)'
+    print 'argv[9] == (OPTIONAL) remove history (TRUE/FALSE), default is FALSE'
+    print 'argv[10] == (OPTIONAL) tracking single particle trajectory (TRUE/FALSE), default is TRUE'
 else:
     dat = loadtxt(sys.argv[1])
     ND = int(sys.argv[3])
@@ -33,15 +35,22 @@ else:
         shear_grad_direction = int(sys.argv[8])
     except:
         Wi_tau_C = 0
-    # inv_PBC_shift_factor_wo_dt = Wi_tau_C*LB;
-    # tmp = box_dimension*int(2*inv_PBC_shift_factor_wo_dt)
-    # ND = 2
-    # NP = 100
+
+    remove_history = 0
+    try:
+        if (sys.argv[9] == 'TRUE'):
+            remove_history == 1
+    except:
+        remove_history = 0
+
+    single_particle_trajectory = 1
+    try:
+        if (sys.argv[10] == 'FALSE'):
+            single_particle_trajectory = 0
+    except:
+        single_particle_trajectory = 1
+        
     Nt = shape(dat)[0]
-    # LB = 10.0
-
-    # dat_conv = zeros([Nt, 2*ND*NP + 1])
-
     
     # def inv_PBC(x_now, x_next, LB):
     #     dX = x_next - x_now
@@ -79,7 +88,8 @@ else:
     #     for j in range(shape(dat)[1]):
     #         dat_record[i,j] = dat[i,j]
 
-    if Wi_tau_C > 0:
+    
+    if Wi_tau_C > 0 and remove_history == 1:
         # it will remove the history of shear flow into coordination change
         for i in range(NP):
             index_shear_direction = 2*ND*i + 1 + shear_direction
@@ -103,40 +113,40 @@ else:
             # in consequence, we have succesful re-mapping
             # note again, the inv_PBC functions are for equilibrium
             ind_k = (k + shear_grad_direction) %ND
+            # ind_k = (k + shear_direction) % ND
             index_Rik = 2*ND*i + 1 + ind_k
             record_pre_coord = 0
             record_now_coord = dat[0, index_Rik]
             coord_shift_factor = 0
+            inv_PBC_shift_factor = 0
             for t in range(1, Nt):
                 record_pre_coord = record_now_coord
                 record_now_coord = dat[t, index_Rik]
                 
                 rev_map_coord = inv_PBC_loop(dat[t-1, index_Rik], dat[t, index_Rik], LB)
                 dat[t, index_Rik] = rev_map_coord
-
                 if Wi_tau_C > 0 and ind_k == shear_grad_direction:
-                    # when simple shear is implemented into the system
+                    # in this part, the shifting in shear_direction (default: x-direction) due to boundary jump on shear_grad_direction (default: y-direction) will be removed
+                    # in consequence of this reverse mapping procedure, the coordinate on shear_direction will NOT track the real particle, but track PARTICLE in PBC BOX (like simulation).
+                    # It is of importance to aware that mechanical perturbation reveals the degenerated cases left-right PBC box condition in shear_grad_direction since it should have different velocity contribution
+                    # Even if our simulation will take configurational Langevin equation, the velocity gradient part make distingushable between particles in its image
                     index_shear_direction = 2*ND*i + 1 + shear_direction
-                    dat[t, index_shear_direction] -= coord_shift_factor
-                    # temporal backup to be on the safe side
-                    # diff = dat_record[t, index_Rik] - dat_record[t-1, index_Rik]
                     diff = record_now_coord - record_pre_coord
                     if abs(diff) > 0.5*LB:
-                        inv_PBC_shift_factor = Wi_tau_C*LB*dat[t, 0] % LB
-                        # coord_shift_factor = Wi_tau_C*record_now_coord*dt
+                        inv_PBC_shift_factor = Wi_tau_C*LB*dat[t, 0] 
                         pre_coord = dat[t, index_shear_direction]
                         for tmp_t in range(t, Nt):
                             # it will remove all the shear history for the future
-                            dat[tmp_t, index_shear_direction] -= sign(diff)*inv_PBC_shift_factor + coord_shift_factor
+                            dat[tmp_t, index_shear_direction] -= sign(diff)*inv_PBC_shift_factor
 
-                            
-                    
-                # if Wi_tau_C > 0 and ind_k == shear_direction:
-                #     # it will remove the history of shear flow
-                #     dat[t, index_Rik] -= coord_shift_factor 
-            # if Wi_tau_C > 0:
-            #     # remove history of simple shear
-            #     for t in range(1, Nt):
-            #         for tmp_t in range(t, Nt):
+                    if(single_particle_trajectory == 1):
+                        # this second part is related with correction for coordination of shear_grad_direction in order to track the real particle case
+                        # the correction factor stored in coord_shift_factor sum over all the history
+                        
+                        diff = rev_map_coord - record_now_coord #differece between transition of y-coordinate
+                        if abs(diff) > 0.5*LB:
+                            coord_shift_factor += sign(diff)*Wi_tau_C*LB*dt # record the history of the coordinate change due to tracking single particle
+                        dat[t, index_shear_direction] += coord_shift_factor
+                        
                         
     savetxt(sys.argv[2], dat)
