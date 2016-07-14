bf8ac0fccb9914997aa6ecbdc3d65a507761b06b
Gun Woo Park <gunwoo.park@unina.it>
2016-07-14 19:36:35 +0200

    [Works in progress] Dumbbell model update and basic test for equilibrium simulation is done. However, nonequilibrium simulation seems to be not working or not clear in virial stress, which should be verified or fixed.
    Former-commit-id: a4e49cb16b370a0355d8df9137fd571f8d02a228

--- lib/file_IO.h
+++ lib/file_IO.h
@@ -28,9 +28,9 @@ public:
     std::cout << "COND CLASS must be contructed with valid fileformat\n";
   }
   COND
-    (char* fn);
+  (char* fn);
   virtual
-    ~COND()
+  ~COND()
   {
     if(arg)
       {
@@ -42,95 +42,95 @@ public:
       }
   }
 
- string& operator()(string option_type);
+  string& operator()(string option_type);
 };
 
 
 class RECORD_DATA
 {
- public:
+public:
   string filename_trajectory, filename_energy, filename_HASH, filename_weight, filename_chain, filename_MC_LOG;
   ofstream traj, ener, hash, weight, chain, MC_LOG;
 
   
   RECORD_DATA()
-    {
-      std::cout << "ERR: Basic Constructor is not supported for RECORD_DATA class\n";
-    }
+  {
+    std::cout << "ERR: Basic Constructor is not supported for RECORD_DATA class\n";
+  }
 
   RECORD_DATA
-    (COND& given_condition)
-    {
-      if(given_condition("output_path")=="FALSE")
-	{
-	  filename_trajectory = (given_condition("filename_base") + ".traj").c_str();
-	  traj.open(filename_trajectory.c_str(), std::ios_base::app);
+  (COND& given_condition)
+  {
+    if(given_condition("output_path")=="FALSE")
+      {
+        filename_trajectory = (given_condition("filename_base") + ".traj").c_str();
+        traj.open(filename_trajectory.c_str(), std::ios_base::app);
       
-	  filename_energy = (given_condition("filename_base") + ".ener").c_str();
-	  ener.open(filename_energy.c_str(), std::ios_base::app);
+        filename_energy = (given_condition("filename_base") + ".ener").c_str();
+        ener.open(filename_energy.c_str(), std::ios_base::app);
       
-	  filename_HASH = (given_condition("filename_base") + ".hash").c_str();
-	  hash.open(filename_HASH.c_str(), std::ios_base::app);
+        filename_HASH = (given_condition("filename_base") + ".hash").c_str();
+        hash.open(filename_HASH.c_str(), std::ios_base::app);
       
-	  filename_weight = (given_condition("filename_base") + ".weight").c_str();
-	  weight.open(filename_weight.c_str(), std::ios_base::app);
+        filename_weight = (given_condition("filename_base") + ".weight").c_str();
+        weight.open(filename_weight.c_str(), std::ios_base::app);
 
-	  if(given_condition("tracking_individual_chain")=="TRUE")
-	    {
-	      filename_chain = (given_condition("filename_base") + ".chain").c_str();
-	      chain.open(filename_chain.c_str(), std::ios_base::app);
-	    }
+        if(given_condition("tracking_individual_chain")=="TRUE")
+          {
+            filename_chain = (given_condition("filename_base") + ".chain").c_str();
+            chain.open(filename_chain.c_str(), std::ios_base::app);
+          }
 
-	  if(given_condition("MC_LOG")=="TRUE")
-	    {
-	      filename_MC_LOG = (given_condition("filename_base") + ".MC_LOG").c_str();
-	      MC_LOG.open(filename_MC_LOG.c_str(), std::ios_base::app);
-	    }
+        if(given_condition("MC_LOG")=="TRUE")
+          {
+            filename_MC_LOG = (given_condition("filename_base") + ".MC_LOG").c_str();
+            MC_LOG.open(filename_MC_LOG.c_str(), std::ios_base::app);
+          }
 	  
-	}
-      else
-	{
-	  filename_trajectory = (given_condition("output_path") + '/' + given_condition("filename_base") + ".traj").c_str();
-	  traj.open(filename_trajectory.c_str(), std::ios_base::app);
+      }
+    else
+      {
+        filename_trajectory = (given_condition("output_path") + '/' + given_condition("filename_base") + ".traj").c_str();
+        traj.open(filename_trajectory.c_str(), std::ios_base::app);
       
-	  filename_energy = (given_condition("output_path") + '/' + given_condition("filename_base") + ".ener").c_str();
-	  ener.open(filename_energy.c_str(), std::ios_base::app);
+        filename_energy = (given_condition("output_path") + '/' + given_condition("filename_base") + ".ener").c_str();
+        ener.open(filename_energy.c_str(), std::ios_base::app);
       
-	  filename_HASH = (given_condition("output_path") + '/' + given_condition("filename_base") + ".hash").c_str();
-	  hash.open(filename_HASH.c_str(), std::ios_base::app);
+        filename_HASH = (given_condition("output_path") + '/' + given_condition("filename_base") + ".hash").c_str();
+        hash.open(filename_HASH.c_str(), std::ios_base::app);
       
-	  filename_weight = (given_condition("output_path") + '/' + given_condition("filename_base") + ".weight").c_str();
-	  weight.open(filename_weight.c_str(), std::ios_base::app);
+        filename_weight = (given_condition("output_path") + '/' + given_condition("filename_base") + ".weight").c_str();
+        weight.open(filename_weight.c_str(), std::ios_base::app);
 
-	  if(given_condition("tracking_individual_chain")=="TRUE")
-	    {
-	      filename_chain = (given_condition("output_path") + '/' + given_condition("filename_base") + ".chain").c_str();
-	      chain.open(filename_chain.c_str(), std::ios_base::app);
-	    }
+        if(given_condition("tracking_individual_chain")=="TRUE")
+          {
+            filename_chain = (given_condition("output_path") + '/' + given_condition("filename_base") + ".chain").c_str();
+            chain.open(filename_chain.c_str(), std::ios_base::app);
+          }
 
-	  if(given_condition("MC_LOG")=="TRUE")
-	    {
-	      filename_MC_LOG = (given_condition("output_path") + '/' + given_condition("filename_base") + ".MC_LOG").c_str();
-	      MC_LOG.open(filename_MC_LOG.c_str(), std::ios_base::app);
-	    }
-	}
-    }
+        if(given_condition("MC_LOG")=="TRUE")
+          {
+            filename_MC_LOG = (given_condition("output_path") + '/' + given_condition("filename_base") + ".MC_LOG").c_str();
+            MC_LOG.open(filename_MC_LOG.c_str(), std::ios_base::app);
+          }
+      }
+  }
   
   ~RECORD_DATA()
-    {
-      if(traj)
-        traj.close();
-      if(ener)
-        ener.close();
-      if(hash)
-        hash.close();
-      if(weight)
-        weight.close();
-      if(chain)
-        chain.close();
-      if(MC_LOG)
-        MC_LOG.close();
-    }
+  {
+    if(traj)
+      traj.close();
+    if(ener)
+      ener.close();
+    if(hash)
+      hash.close();
+    if(weight)
+      weight.close();
+    if(chain)
+      chain.close();
+    if(MC_LOG)
+      MC_LOG.close();
+  }
 };
 
 
