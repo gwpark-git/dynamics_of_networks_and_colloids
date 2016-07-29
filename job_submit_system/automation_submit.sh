2ecc9661a0e2531b9aa05f40e1573c5e4b43766a
Gun Woo Park <gunwoo.park@unina.it>
2016-07-29 19:34:05 +0200

    automation_submit
    Former-commit-id: fdeace399b166bead18783e4f4b48d7f99ee802b

--- job_submit_system/automation_submit.sh
+++ job_submit_system/automation_submit.sh
@@ -4,24 +4,27 @@ then
     echo "Script for job submitting procedure"
     echo "1: given input file"
     echo "2: number of different runs (>1 means the seed will be re-generated)"
-    echo "3 > given data"
+    echo "3: init seed number"
+    echo "4 > given data"
 else
     server=emi2-ce0[1-2].scope.unina.it:8443/cream-pbs-unina_hpc
     # server=recasna-ce01.unina.it:8443/cream-pbs-recas-normal
     file_inp=$1 # input file 
     N_runs=$2
+    init_seed_number=$3
     if [ $N_runs -ne 1 ];
     then
 	echo Number of runs is set with number $N_runs. Seed numbers \in .inp file will be ignored.
     fi
 
+    rm $file_inp.job/*
     mkdir $file_inp.job
     cp ../stochastic_simulation $file_inp.job/stochastic_simulation
     cp stochastic_HEUR.sh $file_inp.job/stochastic_HEUR.sh
     arg_str=''
     if [ $# -gt 2 ]
     then
-	for (( i=3; i<=$#; i++))
+	for (( i=4; i<=$#; i++))
     # it will count and copy the given data files into the job submittion folder
 	do
 	    eval arg_fn=\$$i
@@ -34,7 +37,7 @@ else
     echo $arg_string
     echo '* JOB DESCRIPTION:' >> $file_inp.job/jobs.org
     cat $file_inp >> $file_inp.job/jobs.org
-    python gen_job_files.py $file_inp $N_runs stochastic_HEUR.sh $server $arg_str >> $file_inp.job/jobs.org
+    python gen_job_files.py $file_inp $N_runs $init_seed_number stochastic_HEUR.sh $server $arg_str >> $file_inp.job/jobs.org
     
     cp execute_submit.sh $file_inp.job/execute_submit.sh
     cd $file_inp.job
