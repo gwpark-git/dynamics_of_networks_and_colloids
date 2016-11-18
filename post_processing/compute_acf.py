e8be99fd2f9ecc58538696bbe3a4ed209ed0978b
Gun Woo Park <gunwoo.park@unina.it>
2016-11-18 14:04:56 +0100

    neutralization pp
    Former-commit-id: 11734d1dd24a458c8e21ad653e52d5716d0583f0

--- post_processing/compute_acf.py
+++ post_processing/compute_acf.py
@@ -8,7 +8,8 @@ if size(sys.argv) < 3:
     print 'USAGE :'
     print 'argv[1] == .ener file name'
     print 'argv[2] == output file name'
-    print 'argv[3] == number of cutting variables (HALF will cut half). Default = 0'
+    print 'argv[3] (optional) == number of cutting variables (HALF will cut half). Default = 0.'
+    print 'argv[4] (optional) == number of divisor (average over).'
 else:
     if (sys.argv[1] == sys.argv[2]):
         print 'ERR: input and output have the same file name'
@@ -17,16 +18,39 @@ else:
         ener = loadtxt(sys.argv[1])
         Nt = shape(ener)[0]
         N_cut = 0
-        if size(sys.argv) >= 4:
+        if size(sys.argv) == 4:
             if(sys.argv[3] == 'HALF'):
                 N_cut = Nt/2
             else:
                 N_cut = int(sys.argv[2])
-        print 'compute stress autocorrelation for number of time steps %ld out of %ld'%(N_cut, Nt)
-        corr_con = corr(ener[:,27], ener[:,27])
-        corr_rep = corr(ener[:,21], ener[:,21])
-        corr_con_rep = corr(ener[:,27], ener[:,21])
-        corr_rep_con = corr(ener[:,21], ener[:,27])
+        print 'compute stress autocorrelation for number of time steps %ld out of %ld'%(N_cut, Nt)                
+        if size(sys.argv) == 5:
+
+            N_div = int(sys.argv[4])
+            print 'average over %d blocks'%(N_div)            
+            Nt_block = Nt/N_div
+
+            corr_con = corr(ener[N_cut:N_cut+Nt_block, 27], ener[N_cut:N_cut+Nt_block, 27])
+            corr_rep = corr(ener[N_cut:N_cut+Nt_block, 21], ener[N_cut:N_cut+Nt_block, 21])
+            corr_con_rep = corr(ener[N_cut:N_cut+Nt_block, 27], ener[N_cut:N_cut+Nt_block, 21])
+            corr_rep_con = corr(ener[N_cut:N_cut+Nt_block, 21], ener[N_cut:N_cut+Nt_block, 27])
+
+            for i in range(1, N_div):
+                corr_con = corr(ener[N_cut + i*Nt_block:N_cut+(i+1)*Nt_block, 27], ener[N_cut + i*Nt_block:N_cut+(i+1)*Nt_block, 27])
+                corr_rep = corr(ener[N_cut + i*Nt_block:N_cut+(i+1)*Nt_block, 21], ener[N_cut + i*Nt_block:N_cut+(i+1)*Nt_block, 21])
+                corr_con_rep = corr(ener[N_cut + i*Nt_block:N_cut+(i+1)*Nt_block, 27], ener[N_cut + i*Nt_block:N_cut+(i+1)*Nt_block, 21])
+                corr_rep_con = corr(ener[N_cut + i*Nt_block:N_cut+(i+1)*Nt_block, 21], ener[N_cut + i*Nt_block:N_cut+(i+1)*Nt_block, 27])
+
+            corr_con /= float(N_div)
+            corr_rep /= float(N_div)
+            corr_con_rep /= float(N_div)
+            corr_rep_con /= float(N_div)
+            
+        else:
+            corr_con = (corr(ener[:,27], ener[:,27]) + corr(ener[:,28], ener[:,28]) + corr(ener[:,29], ener[:,29]))/3.
+            corr_rep = (corr(ener[:,21], ener[:,21]) + corr(ener[:,22], ener[:,22]) + corr(ener[:,23], ener[:,23]))/3.
+            corr_con_rep = (corr(ener[:,27], ener[:,21]) + corr(ener[:,28], ener[:,22]) + corr(ener[:,29], ener[:,23]))/3.
+            corr_rep_con = (corr(ener[:,21], ener[:,27]) + corr(ener[:,22], ener[:,28]) + corr(ener[:,23], ener[:,29]))/3.
         dat = zeros([size(corr_con), 6])
         dat[:,0] = ener[:size(corr_con),0] - ener[0, 0]
         dat[:,1] = corr_con + corr_rep + corr_con_rep + corr_rep_con
@@ -35,5 +59,6 @@ else:
         dat[:,4] = corr_con_rep
         dat[:,5] = corr_rep_con
         savetxt(sys.argv[2], dat)
+                
 
 
