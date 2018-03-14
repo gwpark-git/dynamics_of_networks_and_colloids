04e46f3627ea2c01533b02af131f86130d73ee4f
Dr. Gunwoo Park <g.park@fz-juelich.de>
2018-03-14 14:45:26 +0100

    debug: calculating connector vector distribution in Cartesian coordinate system. The methodology seems to be working properly, but need to verify the validity of the code. The coordinate map and index function are the core of the code architecture which are described in the code as a commented lines.
    Former-commit-id: e62611a395acf6a3e93eae7a26ba48285e7552c9

--- post_processing/get_R_hist_file.py
+++ post_processing/get_R_hist_file.py
@@ -5,6 +5,30 @@ from numpy import *
 import sys
 from math import ceil
 
+def map_coordinate_from_index(index_arr, dr, N_dimension, Nr):
+    # Note that Nr represents number of increments for positive direction
+    # total number of index are 1 + 2*Nr
+
+    # the remapping procedure strictly depends on the index handling that is described in index_array function.
+    # here, the remapping procedure is way of inversion where the coordinate remap through an average between two incremental limits.
+    # for instance, the first index (0) will remap to coordinate zero because (-dx/2 + dx/2)/2 = 0
+    # the second index (1) will remap to dx since (dx/2 + (dx/2 + dx))/2 = dx.
+
+    # in the case of negative index, we can identify the negative index through int(index/(Nr + 1))==1 since from 1 to Nr, we have Nr-number of positive index,
+    # then from Nr + 1 to 2 Nr, we have Nr-number of negative index.
+    # note that index for array is always positive while its original mapping (designed from negative to posistive index of array) should be positive integer
+    
+    dr = float(dr)
+    r0 = dr/2.
+    r_arr = zeros(N_dimension)
+    for k in range(N_dimension):
+        if int(index_arr[k]/(Nr + 1)) == 1:
+            r_arr[k] = -dr*index_arr[k]%(Nr + 1)
+        else:
+            r_arr[k] = dr*index_arr[k]
+    return r_arr
+    
+
 def index_array(x, dx):
     # this index function is on the purpose of specialized functionality with negative coordinate
     # for instance, if we have array such as | ... | -1 | 0 | 1 | .... |
@@ -26,6 +50,10 @@ def index_array(x, dx):
     dx = float(dx)
     x0 = dx/2.
     return int(sign(x)*(abs(x) + x0)/dx)
+
+# def index_map_to_1d(I_arr, N_arr):
+#     # I_arr = [Ix, Iy, Iz] (or [Ix, Iy] for 2d)
+#     # N_arr = [Nx, Ny, Nz] which is number of arrays for each directions (or [Nx, Ny] for 2d)
 
 def get_minimum_distance_k_from_x(x, k, box_dimension):
     kd = asarray([k-box_dimension-x, k-x, k+box_dimension-x])
@@ -58,6 +86,7 @@ def hist_R_over_beads(pos, connectivity, box_dimension, hist_R, N_dimension, dr)
     Np, N_dimension = shape(pos)
     # RR = zeros([N_dimension, N_dimension])
     count = 0
+    I_arr = [0, 0, 0] # integer array
     for i in range(Np):
         tmp_index = connectivity[i,i:].nonzero()[0] + i
         #note that nonzero returns tuple. So, the [0] indice will return ndarray
@@ -65,7 +94,18 @@ def hist_R_over_beads(pos, connectivity, box_dimension, hist_R, N_dimension, dr)
             R_j = map_minimum_image_Rj_from_Ri(pos[i,:], pos[j,:], box_dimension)
             R_ij = rel_vec_Rij(pos[i,:], R_j)
             for k in range(N_dimension):
-                hist_R[k, index_array(R_ij[k], dr)] += 1
+                I_arr[k] = index_array(R_ij[k], dr)
+            if (N_dimension==3):
+                hist_R[I_arr[0], I_arr[1], I_arr[2]] += 1
+            elif (N_dimension==2):
+                hist_R[I_arr[0], I_arr[1]] += 1
+            else:
+                print 'wrong dimensionality'
+            
+            # tmp_index_set = zeros(N_dimension)
+            # for k in range(N_dimension):
+            #     tmp_index_set[k] = index_array(R_ij[k], dr)
+            
             count += 1
     return count
 
@@ -153,12 +193,22 @@ else:
     N_direction = int((box_dimension - dr/2.)/dr)
     Nr = 1 + 2*N_direction
     print 'initialized with %d-dimensional case with dr=%f (Nr=%d)'%(N_dimension, dr, Nr)
+    # index 0: middle x
+    # index 1: middle y
+    # index 2: middle z
+    # index 3: intensity
+    # hist_xyz_R = zeros([Nr**N_dimension, N_dimension + 1])
+    # for i in range(Nr):
+        # for j in range(Nr):
+        #     for k in range(Nr):
+        #         index_ijk = k + j*Nr + i*Nr**2
+        #         for d in range(N_dimension):
+        #             hist_xyz_R[index_ijk, d] = 
     # making connector vecotr histogram based on Cartesian coordinate
     # the first array index N_dimension refers the related direction, i.e., hist_R[0] is for x-axis since index 0 refers x
     # the second array index Nr is following the description of index_array function
-    hist_R = zeros([N_dimension, Nr])
-    
-
+    # hist_R = zeros([N_dimension, Nr])
+    hist_R = zeros([Nr, Nr, Nr])
     
     # RR_hist = [] 
     # RR = zeros([N_dimension, N_dimension])
@@ -197,6 +247,7 @@ else:
                         #             for q in range(N_dimension):
                         #                 tmp_RR[p,q] = 
                     except:
+                        print 'breaking line number = ', cnt_lines
                         # RR_hist = asarray(RR_hist)
                         # RR /= float(cnt)
                         # tn = asarray(tn)
@@ -204,8 +255,43 @@ else:
     # dat = zeros([size(tn), N_dimension**2.0 + 1])
     # dat[:,0] = tn
     # dat[:, 1:] = RR
-    dat = hist_R
-    savetxt(fn_out, dat)
+
+    # dat = zeros([Nr**N_dimension, N_dimension + 1])
+
+    # N_count = 0
+    # for i in range(Nr):
+    #     for j in range(Nr):
+    #         for k in range(Nr):
+    #             dat[N_count, 0] = 
+
+    #             dat[N_count, 3] = hist_R[i, j, k]
+                
+
+    # dat = zeros([Nr**N_dimension, N_dimension + 1])
+
+    # note that the following codes are only compatible with 3-dimenional space
+    dat = []
+    # print nonzero(hist_R)
+    tmp_arr = zeros(4)
+    cnt_line = 0
+    for i in range(Nr):
+        for j in range(Nr):
+            for k in range(Nr):
+                if int(hist_R[i,j,k]) > 0:
+                    # this if phrase to reduce the extracted data
+                    # because hist_R will be large-sparse matrix
+                    # most of the data can be ignored since the intensity is zero
+                    tmp_arr[:3] = map_coordinate_from_index([i, j, k], dr, N_dimension, Nr)
+                    tmp_arr[3] = hist_R[i, j, k]
+                    dat.append(tmp_arr.tolist())
+                    # from numpy array to list function is important to prevent the change of data inside arrays
+                    # note that dat was initialized with an empty list in order to use the member function .append
+                    # this procedure has a benefit to compress the sparse matrix hist_R into dat
+                    # then, the dat array will explicitly type change when we call savetxt function
+                    cnt_line += 1
+    # print dat
+    print shape(dat)
+    savetxt(fn_out, asarray(dat))
 
 
     # if N_dimension == 2:
