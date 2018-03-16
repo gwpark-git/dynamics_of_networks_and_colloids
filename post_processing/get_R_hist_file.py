fe2ab3fb83ab084dc2a62f96092e7a9e105731c7
Dr. Gunwoo Park <g.park@fz-juelich.de>
2018-03-16 14:59:59 +0100

    update: neutralization of code and removing unnecessary part. In addition, it make the main check functionality for future compatibility
    Former-commit-id: 9772dcc48b735560b87316362160e773dffbbc46

--- post_processing/get_R_hist_file.py
+++ post_processing/get_R_hist_file.py
@@ -109,17 +109,8 @@ def hist_R_over_beads(pos, connectivity, box_dimension, hist_R, N_dimension, dr)
                 else:
                     print 'wrong dimensionality'
             
-            # tmp_index_set = zeros(N_dimension)
-            # for k in range(N_dimension):
-            #     tmp_index_set[k] = index_array(R_ij[k], dr)
-            
             count += 1
     return count
-
-
-# Def R_over_beads(pos, connectivity, box_dimension):
-#     Np, N_dimension = shape(pos)
-#     R = zeros(
 
 
 def read_traj(f_traj, Np, N_dimension):
@@ -151,218 +142,107 @@ def read_connectivity(f_index, f_weight, Np):
                 break
             else:
                 connectivity[index_i, index_j] = int(tmp_arr_weight[j])
-    # N_cols = shape(str_index_table)[1]
-    # for i in range(Np):
-    #     index_i = int(str_index_table[i][0])
-    #     for j in range(N_cols):
-    #         index_j = int(str_index_table[i][j])
-    #         if index_j == -1:
-    #             break
-    #         else:
-    #             connectivity[index_i, index_j] = int(str_weight_table[i][j])
     return connectivity
     
-# def Np_lines_parsing_index(seq_lines, Np, N_dimension):
-#     connectivity = zeros([Np, Np])
-    
-# def line_parsing_hash(
+if __name__ == "__main__":
+    if size(sys.argv) < 6:
+        print 'USAGE:'
+        print 'argv[1] == base filename'
+        print 'argv[2] == out filename'
+        print 'argv[3] == number of particles'
+        print 'argv[4] == box dimension'
+        print 'argv[5] == number of dimension (current version has symmetric dimensions)'
+        print 'argv[6] == dx_k (current version has symmetric dimensions)'
+        print 'argv[7] == Number of initial cuts (for getting steady-state data)'
+        # print 'argv[8] == strider (to remove serially correlated data)'
+    else:
+        fn_base = sys.argv[1]
+        fn_traj = fn_base + '.traj'
+        fn_index = fn_base + '.hash'
+        fn_weight = fn_base + '.weight'
+        fn_out = fn_base + '_hist_R.dat'
+        Np = int(sys.argv[3])
+        box_dimension = float(sys.argv[4])
+        N_dimension = int(sys.argv[5])
+        N_cols = 2*N_dimension*Np + 1
+        tn = []
 
-if size(sys.argv) < 6:
-    print 'USAGE:'
-    print 'argv[1] == base filename'
-    print 'argv[2] == out filename'
-    print 'argv[3] == number of particles'
-    print 'argv[4] == box dimension'
-    print 'argv[5] == number of dimension (current version has symmetric dimensions)'
-    print 'argv[6] == dx_k (current version has symmetric dimensions)'
-    print 'argv[7] == Number of initial cuts (for getting steady-state data)'
-    # print 'argv[8] == strider (to remove serially correlated data)'
-else:
-    fn_base = sys.argv[1]
-    fn_traj = fn_base + '.traj'
-    fn_index = fn_base + '.hash'
-    fn_weight = fn_base + '.weight'
-    fn_out = fn_base + '_hist_R.dat'
-    Np = int(sys.argv[3])
-    box_dimension = float(sys.argv[4])
-    N_dimension = int(sys.argv[5])
-    N_cols = 2*N_dimension*Np + 1
-    tn = []
+        N_cuts = int(sys.argv[7])
+        # N_strider = int(sys.argv[8])
 
-    N_cuts = int(sys.argv[7])
-    # N_strider = int(sys.argv[8])
+        # read directional increments
+        dr = float(sys.argv[6])
 
-    # read directional increments
-    dr = float(sys.argv[6])
+        # get number of arrays for each directions
+        # this is of important for the following procedure
+        # note that box_dimension/2 is the maximum value of directional coordinate
+        # because of cut-off scheme of the core simulation
+        N_direction = int((box_dimension/2. - dr/2.)/dr)
+        Nr = 1 + 2*N_direction
+        print 'initialized with %d-dimensional case with dr=%f (Nr=%d)'%(N_dimension, dr, Nr)
+        # index 0: middle x
+        # index 1: middle y
+        # index 2: middle z
+        # index 3: intensity
 
-    # get number of arrays for each directions
-    # this is of important for the following procedure
-    # note that box_dimension/2 is the maximum value of directional coordinate
-    # because of cut-off scheme of the core simulation
-    N_direction = int((box_dimension/2. - dr/2.)/dr)
-    Nr = 1 + 2*N_direction
-    print 'initialized with %d-dimensional case with dr=%f (Nr=%d)'%(N_dimension, dr, Nr)
-    # index 0: middle x
-    # index 1: middle y
-    # index 2: middle z
-    # index 3: intensity
-    # hist_xyz_R = zeros([Nr**N_dimension, N_dimension + 1])
-    # for i in range(Nr):
-        # for j in range(Nr):
-        #     for k in range(Nr):
-        #         index_ijk = k + j*Nr + i*Nr**2
-        #         for d in range(N_dimension):
-        #             hist_xyz_R[index_ijk, d] = 
-    # making connector vecotr histogram based on Cartesian coordinate
-    # the first array index N_dimension refers the related direction, i.e., hist_R[0] is for x-axis since index 0 refers x
-    # the second array index Nr is following the description of index_array function
-    # hist_R = zeros([N_dimension, Nr])
-    hist_R = zeros([Nr, Nr, Nr])
-    
-    # RR_hist = [] 
-    # RR = zeros([N_dimension, N_dimension])
-    with open(fn_traj, 'r') as f_traj:
-        with open(fn_index, 'r') as f_index:
-            with open(fn_weight, 'r') as f_weight:
-                cnt = 0
-                cnt_lines = 0
-                if (cnt_lines < N_cuts):
-                    for tmp_iter in xrange(N_cuts):
-                        f_traj.readline()
-                        cnt_lines += 1
-                        for tmp_iter_2 in xrange(Np):
-                            f_index.readline()
-                            f_weight.readline()
-                
-                while(1):
-                    try:
-                        pos = read_traj(f_traj, Np, N_dimension)
-                        connectivity = read_connectivity(f_index, f_weight, Np)
-                        # RR_t = RR_over_beads(pos, connectivity, box_dimension)
-                        if(cnt_lines == N_cuts):
-                            print 'line number %d meet the starting condition'%(N_cuts)
-                        if(cnt_lines >= N_cuts):
-                            if ((cnt_lines - N_cuts)%100 == 0):
-                                print 'currently working with line number %d'%(cnt_lines)
-                            cnt += hist_R_over_beads(pos, connectivity, box_dimension, hist_R, N_dimension, dr)
-                        cnt_lines += 1
-                        # tn.append(cnt)
-                        # RR_hist.append(RR_t.flatten()) # flatten transform 1d from Nd
-                        # RR += RR_t
-                        # cnt += 1
-                        # for i in range(Np):
-                        #     for j in range(Np):
-                        #         tmp_RR = zeros([N_dimension, N_dimension])                                          for p in range(N_dimension):
-                        #             for q in range(N_dimension):
-                        #                 tmp_RR[p,q] = 
-                    except:
-                        print 'breaking line number = ', cnt_lines
-                        # RR_hist = asarray(RR_hist)
-                        # RR /= float(cnt)
-                        # tn = asarray(tn)
-                        break
-    # dat = zeros([size(tn), N_dimension**2.0 + 1])
-    # dat[:,0] = tn
-    # dat[:, 1:] = RR
+        # making connector vecotr histogram based on Cartesian coordinate
+        # the first array index N_dimension refers the related direction, i.e., hist_R[0] is for x-axis since index 0 refers x
+        # the second array index Nr is following the description of index_array function
+        # hist_R = zeros([N_dimension, Nr])
+        hist_R = zeros([Nr, Nr, Nr])
 
-    # dat = zeros([Nr**N_dimension, N_dimension + 1])
+        # RR_hist = [] 
+        # RR = zeros([N_dimension, N_dimension])
+        with open(fn_traj, 'r') as f_traj:
+            with open(fn_index, 'r') as f_index:
+                with open(fn_weight, 'r') as f_weight:
+                    cnt = 0
+                    cnt_lines = 0
+                    if (cnt_lines < N_cuts):
+                        for tmp_iter in xrange(N_cuts):
+                            f_traj.readline()
+                            cnt_lines += 1
+                            for tmp_iter_2 in xrange(Np):
+                                f_index.readline()
+                                f_weight.readline()
 
-    # N_count = 0
-    # for i in range(Nr):
-    #     for j in range(Nr):
-    #         for k in range(Nr):
-    #             dat[N_count, 0] = 
-
-    #             dat[N_count, 3] = hist_R[i, j, k]
-                
-
-    # dat = zeros([Nr**N_dimension, N_dimension + 1])
-
-    # note that the following codes are only compatible with 3-dimenional space
-    dat = []
-    # print nonzero(hist_R)
-    tmp_arr = zeros(4)
-    cnt_line = 0
-    for i in range(Nr):
-        for j in range(Nr):
-            for k in range(Nr):
-                if int(hist_R[i,j,k]) > 0:
-                    # this if phrase to reduce the extracted data
-                    # because hist_R will be large-sparse matrix
-                    # most of the data can be ignored since the intensity is zero
-                    tmp_arr[:3] = map_coordinate_from_index([i, j, k], dr, N_dimension, N_direction)
-                    tmp_arr[3] = hist_R[i, j, k]
-                    dat.append(tmp_arr.tolist())
-                    # from numpy array to list function is important to prevent the change of data inside arrays
-                    # note that dat was initialized with an empty list in order to use the member function .append
-                    # this procedure has a benefit to compress the sparse matrix hist_R into dat
-                    # then, the dat array will explicitly type change when we call savetxt function
-                    cnt_line += 1
-    # print dat
-    print shape(dat)
-    savetxt(fn_out, asarray(dat))
+                    while(1):
+                        try:
+                            pos = read_traj(f_traj, Np, N_dimension)
+                            connectivity = read_connectivity(f_index, f_weight, Np)
+                            # RR_t = RR_over_beads(pos, connectivity, box_dimension)
+                            if(cnt_lines == N_cuts):
+                                print 'line number %d meet the starting condition'%(N_cuts)
+                            if(cnt_lines >= N_cuts):
+                                if ((cnt_lines - N_cuts)%100 == 0):
+                                    print 'currently working with line number %d'%(cnt_lines)
+                                cnt += hist_R_over_beads(pos, connectivity, box_dimension, hist_R, N_dimension, dr)
+                            cnt_lines += 1
+                        except:
+                            print 'breaking line number = ', cnt_lines
+                            break
+        # note that the following codes are only compatible with 3-dimenional space
+        dat = []
+        # print nonzero(hist_R)
+        tmp_arr = zeros(4)
+        cnt_line = 0
+        for i in range(Nr):
+            for j in range(Nr):
+                for k in range(Nr):
+                    if int(hist_R[i,j,k]) > 0:
+                        # this if phrase to reduce the extracted data
+                        # because hist_R will be large-sparse matrix
+                        # most of the data can be ignored since the intensity is zero
+                        tmp_arr[:3] = map_coordinate_from_index([i, j, k], dr, N_dimension, N_direction)
+                        tmp_arr[3] = hist_R[i, j, k]
+                        dat.append(tmp_arr.tolist())
+                        # from numpy array to list function is important to prevent the change of data inside arrays
+                        # note that dat was initialized with an empty list in order to use the member function .append
+                        # this procedure has a benefit to compress the sparse matrix hist_R into dat
+                        # then, the dat array will explicitly type change when we call savetxt function
+                        cnt_line += 1
+        # print dat
+        print shape(dat)
+        savetxt(fn_out, asarray(dat))
 
 
-    # if N_dimension == 2:
-    #     # ref_xx = asarray([[0, RR[0, 0]],
-    #     #                   [tn[-1], RR[0, 0]]])
-    #     # ref_xy = asarray([[0, RR[0, 1]],
-    #     #                   [tn[-1], RR[0, 1]]])
-    #     # ref_yy = asarray([[0, RR[1, 1]],
-    #     #                   [tn[-1], RR[1, 1]]])
-    #     # # plt.clf()
-    #     # # plt.ion()
-    #     # plt.plot(tn, RR_hist[:,0], 'b-', label = r'$\langle R_xR_x\rangle_{p}$')
-    #     # plt.plot(tn, RR_hist[:,3], 'r-', label = r'$\langle R_yR_y\rangle_{p}$')
-    #     # plt.plot(tn, RR_hist[:,1], 'g-', label = r'$\langle R_xR_y\rangle_{p}$')
-    #     # plt.plot(ref_xx[:,0], ref_xx[:,1], 'b:', linewidth=3, label = r'$\langle R_xR_x\rangle_{p, t}$ = %6.2f'%(RR[0,0]))
-    #     # plt.plot(ref_yy[:,0], ref_yy[:,1], 'r:', linewidth=3, label = r'$\langle R_yR_y\rangle_{p, t}$ = %6.2f'%(RR[1,1]))
-    #     # plt.plot(ref_xy[:,0], ref_xy[:,1], 'g:', linewidth=3, label = r'$\langle R_xR_y\rangle_{p, t}$ = %6.2f'%(RR[0,1]))
-
-    #     # font_prop = FontProperties()
-    #     # font_prop.set_size('small')
-
-    #     # plt.legend(loc = 'center left', ncol=2, prop=font_prop)
-    #     # plt.xlabel('time steps')
-    #     # plt.ylabel(r'$\mathbf{RR}$ averaged over all connection pairs, $\langle \mathbf{R}\mathbf{R}\rangle_{p}$')
-    #     # plt.grid('on')
-    #     # plt.savefig(fn_out)
-    #     # plt.show()
-
-    # else:
-    #     ref_xx = asarray([[0, RR[0, 0]],
-    #                       [tn[-1], RR[0, 0]]])
-    #     ref_xy = asarray([[0, RR[0, 1]],
-    #                       [tn[-1], RR[0, 1]]])
-    #     ref_xz = asarray([[0, RR[0, 2]],
-    #                       [tn[-1], RR[0, 2]]])
-    #     ref_yy = asarray([[0, RR[1, 1]],
-    #                       [tn[-1], RR[1, 1]]])
-    #     ref_yz = asarray([[0, RR[1, 2]],
-    #                       [tn[-1], RR[1, 2]]])
-    #     ref_zz = asarray([[0, RR[2, 2]],
-    #                       [tn[-1], RR[2, 2]]])
-    #     plt.plot(tn, RR_hist[:,0], 'b-', label = r'$\langle R_xR_x\rangle_{p}$')
-    #     plt.plot(tn, RR_hist[:,4], 'r-', label = r'$\langle R_yR_y\rangle_{p}$')
-    #     plt.plot(tn, RR_hist[:,8], 'g-', label = r'$\langle R_zR_z\rangle_{p}$')
-    #     plt.plot(tn, RR_hist[:,1], '-', color='cyan', label = r'$\langle R_xR_y \rangle_{p}$')
-    #     plt.plot(tn, RR_hist[:,2], '-', color='purple', label = r'$\langle R_xR_z \rangle_{p}$')
-    #     plt.plot(tn, RR_hist[:,5], '-', color='yellow', label = r'$\langle R_yR_z \rangle_{p}$')
-    #     # plt.plot(tn, RR_hist[:,3], 'r-', label = r'$\langle R_yR_y\rangle_{p}$')
-    #     # plt.plot(tn, RR_hist[:,1], 'g-', label = r'$\langle R_xR_y\rangle_{p}$')
-    #     plt.plot(ref_xx[:,0], ref_xx[:,1], 'b:', linewidth=3, label = r'$\langle R_xR_x\rangle_{p, t}$ = %6.4f'%(RR[0,0]))
-    #     plt.plot(ref_yy[:,0], ref_yy[:,1], 'r:', linewidth=3, label = r'$\langle R_yR_y\rangle_{p, t}$ = %6.4f'%(RR[1,1]))
-    #     plt.plot(ref_zz[:,0], ref_zz[:,1], 'g:', linewidth=3, label = r'$\langle R_zR_z\rangle_{p, t}$ = %6.4f'%(RR[2,2]))
-    #     plt.plot(ref_xx[:,0], ref_xy[:,1], ':', color='cyan', linewidth=3, label = r'$\langle R_xR_y\rangle_{p, t}$ = %6.4f'%(RR[0,1]))
-    #     plt.plot(ref_yy[:,0], ref_xz[:,1], ':', color='purple', linewidth=3, label = r'$\langle R_xR_z\rangle_{p, t}$ = %6.4f'%(RR[0,2]))
-    #     plt.plot(ref_zz[:,0], ref_yz[:,1], ':', color='yellow', linewidth=3, label = r'$\langle R_yR_z\rangle_{p, t}$ = %6.4f'%(RR[1,2]))
-
-    #     font_prop = FontProperties()
-    #     font_prop.set_size('small')
-
-    #     plt.legend(loc = 'center right', ncol=2, prop=font_prop)
-    #     plt.xlabel('time steps')
-    #     plt.ylabel(r'$\mathbf{RR}$ averaged over all connection pairs, $\langle \mathbf{R}\mathbf{R}\rangle_{p}$')
-    #     plt.grid('on')
-    #     plt.savefig(fn_out)
-        
